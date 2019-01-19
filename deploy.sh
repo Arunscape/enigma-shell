b08c36b7a873bb7d5be6340f2841851b0cd4fff4
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-19 12:49:11 -0700

    add file for ci and automatic deployment

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+# install AWS SDK
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+REPO_NAME=travisci-test
+
+# install necessary dependency for ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+# install ecs-deploy
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
+  sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# login AWS ECR
+#eval $(aws ecr get-login --region us-east-1)
+
+# or login DockerHub
+docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
+
+# build the docker image and push to an image repository
+docker build -t $REPO_NAME .
+docker tag $REPO_NAME $DOCKER_HUB_USER/$REPO_NAME
+docker push $DOCKER_HUB_USER/$REPO_NAME
+
+# update an AWS ECS service with the new image
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
