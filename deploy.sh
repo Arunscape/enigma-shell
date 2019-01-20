4653e7bf5f5aae8d16de8c780f407beceb5dce04
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-19 19:56:30 -0700

    pls

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,4 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ./deploy_key
-cat ./deploy_key
 ssh -i ./deploy_key $SERVER_IP "docker stop $CONTAINER_NAME; docker pull $DOCKER_REPO; docker run -p 80:80 --rm $DOCKER_REPO"
