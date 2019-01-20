7dc4b57f9f2f15b8051907a2e79986ece6d8d74b
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-19 18:30:10 -0700

    LET'S SEE IF THIS WORKS

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@ SERVER_IP=35.233.140.84
 CONTAINER_NAME=klt-enigma-shell-mhxn
 DOCKER_REPO=arunscape/enigma-shell
 
-ssh $SERVER_IP "docker stop $CONTAINER_NAME; docker pull $DOCKER_REPO; docker run -p 80:80 --rm $DOCKER_REPO"
+eval "$(ssh-agent -s)"
+chmod 600 ./deploy_key
+echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ./deploy_key
+ssh -i ./deploy_key $SERVER_IP "docker stop $CONTAINER_NAME; docker pull $DOCKER_REPO; docker run -p 80:80 --rm $DOCKER_REPO"
