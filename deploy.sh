94f34835f930489c2c8c3fb26b79b035afb8e3ee
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-19 19:26:40 -0700

    PLZ WORK

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ SERVER_IP=35.233.140.84
 CONTAINER_NAME=klt-enigma-shell-mhxn
 DOCKER_REPO=arunscape/enigma-shell
 
+set -x
 eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 echo -e "Host $SERVER_IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
