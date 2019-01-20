f57e4b29b91a67df608ae0e3acc24a52219a08d3
Amber Wolinski <33074030+awolinski@users.noreply.github.com>
2019-01-19 22:15:45 -0700

    Add files via upload

---
+++ Console.js
@@ -0,0 +1,8 @@
+function toggleNavigation(id) {
+        document.getElementById(id).hidden = !document.getElementById(id).hidden;
+  }
+
+function getTime() {
+    var time = new Date();
+    console.log(time);
+}
