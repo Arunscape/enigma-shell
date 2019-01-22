8c695274fd9702c73ce29a717e9ce1ef74a7acfa
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-21 20:22:39 -0700

    start over with vue lol

--- amber_stuff/js/Console.js
+++
@@ -1,7 +0,0 @@
-function toggleNavigation(id) {
-    var x = document.getElementById(id);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }  }
--- src/components/Console.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-
-export default () => (
-  <>
-    <TextField />
-  </>
-);
